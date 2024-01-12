6649e2ab7e8a28d9d3e8ad8e2f3f0aaffd83af2e
Dan Kramer <dan.r.kramer@gmail.com>
2024-01-11 23:12:47 -0800

    Some progress and ideas but stuck on PyBank Greatest Inc/Dec info.

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,7 +28,6 @@ with open(csvpath) as csvfile:
     # The file has a header, so we need the read the header row first.
 
     csv_header = next(csvreader)
-    print(f'CSV Header: {csv_header}')
 
     # Create a counter to sum the months.
     total_months = 0
@@ -38,6 +37,10 @@ with open(csvpath) as csvfile:
     pl_change = 0
     # Create an empty list to record each value of change during the loop.
     pl_list = []
+
+    date_list = []
+
+    full_list = []
 
     # Loop through the data to capture needed values:
     for row in csvreader:
@@ -52,6 +55,11 @@ with open(csvpath) as csvfile:
     # After calculating the difference, reset the pl_change variable at the end of each loop to be
     # the correct value so the loop calculates an accurate difference from the next row.
         pl_change = (row[1])
+
+        date_list.append(row[0])
+
+        full_list.append(row)
+
 
     # After the loop completes, the first entry added to the list is technically inaccurate (it adds
     # a value that wasn't subtracted from anything), so it needs to be removed from the list via the pop function.
@@ -70,9 +78,32 @@ with open(csvpath) as csvfile:
 
     print(f'total ${net_total}')
 
+    
     # When printing the average Profit/Loss, the expected format is rounded to two decimal places, 
     # which requires the round function.
     print(round(pl_total,2))
 
 
+    max = int(max(pl_list))
+
+    min = int(min(pl_list))
+
+    print(max)
+
+    print(min)
+
+    print(date_list)
+
+    for row in full_list:
+
+        if row == (str(date_list), int(max)):
     
+            print("Hello")
+
+            
+            #print(f'{1- (row[0])} (${row[1]})')
+        
+        #elif 
+
+        #print(f'Greatest Decrease in Profits: {row[0]} (${row[1]})')
+            
